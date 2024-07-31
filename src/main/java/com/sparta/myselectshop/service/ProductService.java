8b5da0ee88dccb4025be95beb8eb29e9f303d2b0
coding-911 <ye22im@gmail.com>
2024-07-31 14:11:39 +0900

    관심 상품 등록

---
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -0,0 +1,22 @@
+package com.sparta.myselectshop.service;
+
+import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
+        System.out.println("products 서비스 진입");
+        Product product = productRepository.save(new Product(requestDto));
+        return new ProductResponseDto(product);
+    }
+}
