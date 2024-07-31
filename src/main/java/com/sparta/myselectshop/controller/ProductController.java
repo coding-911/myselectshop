5a637d290f492160a98894571f589f1a38496f02
coding-911 <ye22im@gmail.com>
2024-07-31 14:27:11 +0900

    관심 상품 조회 api

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -7,6 +7,8 @@ import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -28,5 +30,11 @@ public class ProductController {
         return productService.updateProduct(id, requestDto);
     }
 
+    // 관심 상품 조회하기
+    @GetMapping("/products")
+    public List<ProductResponseDto> getProducts() {
+        // 응답 보내기
+        return productService.getProducts();
+    }
 
 }
